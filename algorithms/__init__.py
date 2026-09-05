cbfede2869fc44e7c7d1b61b37200e948ef89e65
Mahyad <saedpanahmahyad@gmail.com>
2026-09-05 15:57:51 +0330

    register ACTA algorithm

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,11 +1,19 @@
 from algorithms.ACON import ACON
+from algorithms.ACTA import ACTA
 
-__all__ = ['ACON']
+__all__ = [
+    "ACON",
+    "ACTA",
+]
+
 
 def get_algorithm_class(algorithm_name):
     """Return the algorithm class with the given name."""
     if algorithm_name not in globals():
-        raise NotImplementedError("Algorithm not found: {}".format(algorithm_name))
+        raise NotImplementedError(
+            "Algorithm not found: {}".format(
+                algorithm_name
+            )
+        )
+
     return globals()[algorithm_name]
-
-
