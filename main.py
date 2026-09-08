f90f2328864c6c82b486a5c1ba6d2c735bb84df7
Mahyad <saedpanahmahyad@gmail.com>
2026-09-08 21:19:52 +0330

    update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ parser.add_argument('--run_description', default='ACON', type=str, help='name of
 parser.add_argument('--da_method', default='ACON', type=str)
 
 # ========= Select the DATASET ==============
-parser.add_argument('--data_path', default=r"D:\ACTA\data", type=str, help='Path containing dataset')
+parser.add_argument('--data_path', default="/content/ACTA/data", type=str, help='Path containing dataset')
 parser.add_argument('--dataset', default='UCIHAR',type=str)
 
 # ========= Select the BACKBONE ==============
@@ -38,12 +38,20 @@ parser.add_argument('--end', type=int, default=None)
 parser.add_argument('-p','--print-freq', type=int, default=10, help='each epoch print num_epochs/p times ')
 parser.add_argument('--num_workers', type=int, default=2)
 parser.add_argument('--shuffle', action='store_true', help='whether shuffle the train dataset')
-# ACTA
+
+# ========= ACTA / ACTA-v2 ===================
 parser.add_argument(
     '--acta_mode',
     type=str,
     default='ACTA',
-    choices=['ACTA', 'UTA', 'ClassShuffle']
+    choices=[
+        'ACTA',
+        'RFT',
+        'UNIFORM',
+        'LegacyACTA',
+        'UTA',
+        'ClassShuffle',
+    ]
 )
 
 parser.add_argument(
@@ -54,6 +62,12 @@ parser.add_argument(
 
 parser.add_argument(
     '--acta_beta',
+    type=float,
+    default=1.0
+)
+
+parser.add_argument(
+    '--acta_eta',
     type=float,
     default=1.0
 )
@@ -87,6 +101,7 @@ parser.add_argument(
     type=str,
     default='semantic_packages'
 )
+
 parser.add_argument('--phase', default='train', type=str)
 parser.add_argument('--test_model_prefix', type=str)
 
@@ -116,11 +131,9 @@ if args.debug:
 
 
 if __name__ == "__main__":
-    
+
     trainer = trainers.da_trainer(args)
     if args.phase == 'test':
         trainer.test()
     else:
         trainer.train()
-    
-   
